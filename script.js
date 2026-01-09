6d225b8b3082e99fdb9ac18ba4209095860fa6ce
Guilherme-Bisof <gui_bisof@hotmail.com>
2026-01-09 12:21:38 -0300

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -27,33 +27,120 @@ function menuMobol() {
 
 menuMobol();
 
-const divExperiencia = document.querySelectorAll(".experience_content div");
-const liExperiencia = document.querySelectorAll(".experience_content ul li");
-const divEducation = document.querySelectorAll(".education_content div");
-const liEducation = document.querySelectorAll(".education_content ul li");
+function sobreMim() {
+  const divExperiencia = document.querySelectorAll(".experience_content div");
+  const liExperiencia = document.querySelectorAll(".experience_content ul li");
+  const divEducation = document.querySelectorAll(".education_content div");
+  const liEducation = document.querySelectorAll(".education_content ul li");
 
-function slideShow(index) {
-  divExperiencia.forEach((div) => {
-    div.classList.remove("ativo");
+  divExperiencia[0].classList.add("ativo");
+  divEducation[0].classList.add("ativo");
+  liExperiencia[0].classList.add("ativo");
+  liEducation[0].classList.add("ativo");
+
+  function slideShow(index) {
+    divExperiencia.forEach((div) => {
+      div.classList.remove("ativo");
+    });
+    liExperiencia.forEach((botao) => {
+      botao.classList.remove("ativo");
+    });
+    divExperiencia[index].classList.add("ativo");
+    liExperiencia[index].classList.add("ativo");
+  }
+
+  function slideShow2(index) {
+    divEducation.forEach((div) => {
+      div.classList.remove("ativo");
+    });
+    liEducation.forEach((botao) => {
+      botao.classList.remove("ativo");
+    });
+    divEducation[index].classList.add("ativo");
+    liEducation[index].classList.add("ativo");
+  }
+
+  liExperiencia.forEach((event, index) => {
+    event.addEventListener("click", () => {
+      slideShow(index);
+    });
   });
-  liExperiencia.forEach((botao) => {
-    botao.classList.remove("ativo");
+
+  liEducation.forEach((event, index) => {
+    event.addEventListener("click", () => {
+      slideShow2(index);
+    });
   });
-  divExperiencia[index].classList.add("ativo");
-  liExperiencia[index].classList.add("ativo");
 }
 
-function slideShow(index) {
-  divEducation.forEach((div) => {
-    div.classList.remove("ativo");
+sobreMim();
+
+const listaALL = document.querySelectorAll(".projects_armazenamento ul li");
+const buttonGeral = document.querySelectorAll(".projects_models ul li");
+const buttonAll = document.querySelectorAll(".projects_navegacao .all");
+
+function removeClick(index) {
+  buttonGeral.forEach((item) => {
+    item.classList.remove("ativo");
   });
-  liEducation.forEach((botao) => {
-    botao.classList.remove("ativo");
-  });
-  divEducaliEducation[index].classList.add("ativo");
-  liEducation[index].classList.add("ativo");
+  buttonGeral[index].classList.add("ativo");
 }
 
-liExperiencia.forEach((event, index)=> {
-  event.addEventListener()
-})
+buttonGeral.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    removeClick(index);
+  });
+});
+
+function showLista(lista, buttom = "all") {
+  lista.forEach((item) => {
+    item.classList.remove("ativo");
+  });
+  if (buttom == "design") {
+    lista[0].classList.add("ativo");
+    lista[1].classList.add("ativo");
+  }
+
+  if (buttom == "graphic") {
+    lista[2].classList.add("ativo");
+    lista[3].classList.add("ativo");
+  }
+
+  if (buttom == "website") {
+    lista[4].classList.add("ativo");
+    lista[5].classList.add("ativo");
+    lista[6].classList.add("ativo");
+    lista[7].classList.add("ativo");
+  }
+
+  if (buttom == "all") {
+    lista[0].classList.add("ativo");
+    lista[1].classList.add("ativo");
+    lista[2].classList.add("ativo");
+    lista[3].classList.add("ativo");
+    lista[4].classList.add("ativo");
+    lista[5].classList.add("ativo");
+    lista[6].classList.add("ativo");
+    lista[7].classList.add("ativo");
+  }
+}
+buttonGeral.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    let currentButton = e.target;
+    if (currentButton.classList.contains("all")) {
+      showLista(listaALL);
+    }
+
+    if (currentButton.classList.contains("design")) {
+      showLista(listaALL, "design");
+    }
+
+    if (currentButton.classList.contains("graphic")) {
+      showLista(listaALL, "graphic");
+    }
+
+    if (currentButton.classList.contains("website")) {
+      showLista(listaALL, "website");
+    }
+  });
+});
